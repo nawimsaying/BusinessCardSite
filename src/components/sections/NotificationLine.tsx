73ded8d0869e97e3a68eeca5302dd95def021255
xorti <yranidrop@gmail.com>
2024-02-05 19:11:48 +0300

    Add NotificationLine

---
+++ src/components/sections/NotificationLine.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import styles from '/src/components/sections/NotificationLine.module.css'
+
+const NotificationLine = () => {
+    return (
+        <>
+            <div className={styles.frame}>
+                <div className={styles.image_container_left}>
+                    <img src='public/images/GridNotificationLine.png'></img>
+                </div>
+
+                <div className={styles.image_container_right}>
+                    <img src='public/images/Grid2NotificationLine.png'></img>
+                </div>
+
+                <div className={styles.text_container}>
+                    <p className={styles.text}>
+                        Создаем виртуальные пространства для вашего успеха!
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NotificationLine
