99573c8e1816cb3562ef6de665d88ae574922fc9
Daniil <istb-211_932686@volsu.ru>
2024-02-21 17:43:19 +0300

    WhyUs section is ready

--- src/components/sections/WhyUs.tsx
+++ src/components/sections/WhyUs.tsx
@@ -5,28 +5,34 @@ import styles from './WhyUs.module.css'
 const WhyUs = () => {
     return (
         <>
-            <div className={styles.background}>
-                <h1 className={styles.title}>
-                    Почему именно мы?
-                </h1>
+            <div className={styles.info_screen}>
+                <div className={styles.info_text}>
+                    <h1>Почему именно мы?</h1>
+                </div>
 
                 <div className={styles.container}>
-                    <div className={styles.block}>
-                        <div className={styles.img_block}>
-                            <img className={styles.blockimg} src='public/images/whyus_1.png'></img>
-                        </div>
-
-                        <div className={styles.block_container}>
-                            <h4 className={styles.block_title}>Команда</h4>
+                    <div className={styles.info_block}>
+                        <div className={styles.info_block_item}>
+                            <img className={styles.image} src='public\images\whyus_1.png' alt='4'></img>
+                            <h2>Команда</h2>
+                            <h4 className={styles.block_small_text}>Молодая команда опытных разработчиков, знающих свое дело, обязательно поможет достичь желаемого результата.</h4>
                         </div>
                     </div>
 
-                    <div className={styles.block}>
-
+                    <div className={styles.info_block}>
+                        <div className={styles.info_block_item}>
+                            <img className={styles.image} src='public\images\whyus_2.png' alt='4'></img>
+                            <h2>Выгода</h2>
+                            <h4 className={styles.block_small_text}>Конкурентноспособные предложения с приятными ценниками и адекватными сроками разработки.</h4>
+                        </div>
                     </div>
 
-                    <div className={styles.block}>
-
+                    <div className={styles.info_block}>
+                        <div className={styles.info_block_item}>
+                            <img className={styles.image} src='public\images\whyus_3.png' alt='4'></img>
+                            <h2>Гарантия</h2>
+                            <h4 className={styles.block_small_text}>Бесплатные правки и доработка до 100% результата.</h4>
+                        </div>
                     </div>
                 </div>
             </div>
