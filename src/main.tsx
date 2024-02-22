53968baee6b72d451ec19500e2f67c619d51acb5
Daniil <istb-211_932686@volsu.ru>
2024-02-22 19:30:59 +0300

    Almost ready section Footer (not clickable)

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import {BrowserRouter} from "react-router-dom";
 
 import App from './App.tsx'
 import NotificationLine from './components/sections/NotificationLine.tsx'
 import NavBar from './components/UI/header/NavBar.tsx'
-import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter.tsx";
 import Header from './components/sections/Header.tsx'
 import DevApproach from './components/sections/DevApproach.tsx'
 import WhyUs from './components/sections/WhyUs.tsx'
 import Contact from './components/sections/Contact.tsx'
+import Footer from './components/sections/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Contact/>
+        <Footer/>
     </React.StrictMode>
 )
