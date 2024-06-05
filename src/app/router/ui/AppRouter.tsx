997e267ecbcb1beaa5bcbfe61bdae999db37d33d
Velp <90183727+Velp6268@users.noreply.github.com>
2024-06-05 11:49:14 +0300

    changed components for FSD methodology

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,11 +1,12 @@
 import React, {Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {routeConfig} from "shared/config/routeConfig/routeConfig.tsx";
+import {BackgroundUIMock} from "shared/config";
 
 
 const AppRouter: React.FC = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<BackgroundUIMock/>}>
             <Routes>
                 {Object.values(routeConfig).map(({element, path}) => (
                     <Route
